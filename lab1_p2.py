b83a645d197772e27cedf3c85ff2161fca191525
Nikhil Reji <117935874+nikhilreji11@users.noreply.github.com>
2025-06-27 19:04:02 +0530

    Add files via upload

---
+++ lab1_p2.py
@@ -0,0 +1,9 @@
+stud1=int(input("Enter the student 1 marks:"))
+stud2=int(input("Enter the student 2 marks:"))
+stud3=int(input("Enter the student 3 marks:"))
+stud4=int(input("Enter the student 4 marks:"))
+stud5=int(input("Enter the student 5 marks:"))
+sum=stud2+stud1+stud3+stud4+stud5
+avg=sum/5
+print(avg)
+                
